d1456d68d33be8cb6d95ee699cd988b539692d50
squaden <druzh.ds@gmail.com>
2019-08-23 17:06:02 +0300

    Initial version

---
+++ app/models/question.rb
@@ -0,0 +1,7 @@
+class Question < ApplicationRecord
+
+  belongs_to :user
+
+  validates :text, :user, presence: true
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,51 @@
+require 'openssl'
+
+class User < ApplicationRecord
+  # параметры для работы модуля шифрования паролей
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+
+  attr_accessor :password
+
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      # создаём т.н. "соль" - рандомная строка, усложняющая задачу хакерам
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      # создаём хэш пароля - длинная уникальная строка, из которой невозможно восстановить
+      # исходный пароль
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS,
+                                   DIGEST.length, DIGEST))
+    end
+  end
+
+  # служебный метод, преобразующий бинарную строку в 16-ричный формат для удобства
+  # хранения
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email) # сперва находим кандидата по email
+
+    # Сравнивается только password_hash, а оригинальный пароль никогда и нигде не
+    # сохраняется
+    if user.present? && user.password_hash == User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS,
+                                 DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
+end
