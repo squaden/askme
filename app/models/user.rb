11a96fd4e7b4c8d5a4af80cf1f495629b9db17a3
squaden <druzh.ds@gmail.com>
2019-08-25 18:32:33 +0300

    Add downcase for :username before validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,53 +7,57 @@ class User < ApplicationRecord
 
   has_many :questions
 
-  validates :username,
-            presence: true,
-            uniqueness: true,
-            length: { maximum: 40 },
-            format: { with: /\A\w+\z/i }
-  validates :email,
-            presence: true,
-            uniqueness: true,
-            format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  before_validation :downcase_username
 
+  validates :username, presence: true,
+                       uniqueness: true,
+                       length: { maximum: 40 },
+                       format: { with: /\A\w+\z/ }
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 
-  # attr_accessor :password
-  #
-  # validates_presence_of :password, on: :create
-  # validates_confirmation_of :password
-  #
-  # before_save :encrypt_password
-  #
-  # def encrypt_password
-  #   if self.password.present?
-  #     # создаём т.н. "соль" - рандомная строка, усложняющая задачу хакерам
-  #     self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-  #     # создаём хэш пароля - длинная уникальная строка, из которой невозможно восстановить
-  #     # исходный пароль
-  #     self.password_hash = User.hash_to_string(
-  #       OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS,
-  #                                  DIGEST.length, DIGEST))
-  #   end
-  # end
+  attr_accessor :password
+
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      # создаём т.н. "соль" - рандомная строка, усложняющая задачу хакерам
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      # создаём хэш пароля - длинная уникальная строка, из которой невозможно восстановить
+      # исходный пароль
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS,
+                                   DIGEST.length, DIGEST))
+    end
+  end
 
   # служебный метод, преобразующий бинарную строку в 16-ричный формат для удобства
   # хранения
-  # def self.hash_to_string(password_hash)
-  #   password_hash.unpack('H*')[0]
-  # end
-  #
-  # def self.authenticate(email, password)
-  #   user = find_by(email: email) # сперва находим кандидата по email
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
 
-    # Сравнивается только password_hash, а оригинальный пароль никогда и нигде не
-    # сохраняется
-#     if user.present? && user.password_hash == User.hash_to_string(
-#       OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS,
-#                                  DIGEST.length, DIGEST))
-#       user
-#     else
-#       nil
-#     end
-#   end
+  def self.authenticate(email, password)
+    user = find_by(email: email) # сперва находим кандидата по email
+
+    Сравнивается только password_hash, а оригинальный пароль никогда и нигде не
+    сохраняется
+    if user.present? && user.password_hash == User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS,
+                                 DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
+
+  private
+  def downcase_username
+    self.username.downcase!
+  end
 end
