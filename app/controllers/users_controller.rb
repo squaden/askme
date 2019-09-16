498a83f31c9db79c339f0919b445ec66bb2111fb
squaden <druzh.ds@gmail.com>
2019-09-16 12:05:15 +0300

    Add edit background color to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,6 +59,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :name,
-                                 :username, :avatar_url)
+                                 :username, :avatar_url, :background_color)
   end
 end
